6a26d7034eff6c159d7d23e856177a0fe38d12e0
AKAxedx <1365957941@qq.com>
2023-11-21 19:24:23 +0800

    把屎改了

---
+++ fjoj/src/layouts/CourseLayout.vue
@@ -0,0 +1,208 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-container>
+        <div id="menu" style="height: 500px">
+          <el-aside default-openeds="1" width="400px">
+            <el-col :span="12">
+              <el-menu
+                  active-text-color="#ffd04b"
+                  background-color="#545c64"
+                  class="el-menu-vertical-demo"
+                  default-active="2"
+                  text-color="#fff"
+                  @close="handleClose"
+                  @open="handleOpen"
+              >
+                <el-sub-menu index="1">
+                  <template #title>
+                    <el-icon>
+                      <Location/>
+                    </el-icon>
+                    <span>课程</span>
+                  </template>
+                  <el-menu-item index="1-1"  @click="Click(course[0])"
+                  >语文
+                  </el-menu-item>
+                  <el-menu-item index="1-2" @click="Click(course[1])"
+                  >数学
+                  </el-menu-item>
+                  <el-menu-item index="1-3" @click="Click(course[2])"
+                  >英语
+                  </el-menu-item>
+                  <el-sub-menu index="1-4">
+                    <template #title>其它</template>
+                    <el-menu-item index="1-4-1" @click="Click(course[3])"
+                    >物理
+                    </el-menu-item>
+                    <el-menu-item index="1-4-2" @click="Click(course[4])"
+                    >化学
+                    </el-menu-item>
+                    <el-menu-item index="1-4-3" @click="Click(course[5])"
+                    >政治
+                    </el-menu-item>
+                    <el-menu-item index="1-4-4" @click="Click(course[6])"
+                    >历史
+                    </el-menu-item>
+                  </el-sub-menu>
+                </el-sub-menu>
+                <el-sub-menu index="2">
+                  <template #title>
+                    <el-icon>
+                      <icon-menu/>
+                    </el-icon>
+                    <span>年级</span>
+                  </template>
+                  <el-menu-item index="2-1-1" @click="Click(course[7])"
+                  >小学
+                  </el-menu-item>
+                  <el-menu-item index="2-1-2" @click="Click(course[8])"
+                  >初中
+                  </el-menu-item>
+                  <el-menu-item index="2-1-3" @click="Click(course[9])"
+                  >高中
+                  </el-menu-item>
+                </el-sub-menu>
+                <el-menu-item index="3">
+                  <el-icon>
+                    <setting/>
+                  </el-icon>
+                  <span>设置</span>
+                </el-menu-item>
+                <el-menu-item index="4">
+                  <span></span>
+                </el-menu-item>
+              </el-menu>
+            </el-col>
+          </el-aside>
+        </div>
+        <el-container>
+          <el-header class="header">
+            <el-menu
+                :default-active="activeIndex"
+                :ellipsis="false"
+                mode="horizontal"
+                @select="handleSelect"
+            >
+              <el-menu-item class="mymenu" index="1">学习课程</el-menu-item>
+            </el-menu>
+          </el-header>
+          <a-divider/>
+          <el-main class="main">
+            <a-space class="search" direction="vertical" size="large">
+              <a-input-search
+                  :style="{ width: '320px' }"
+                  placeholder="Please enter something"
+                  search-button
+              >
+                <template> Search</template>
+              </a-input-search>
+            </a-space>
+            <router-view/>
+          </el-main>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import {Location, Menu as IconMenu, Setting} from "@element-plus/icons-vue";
+import PrimaryView from "@/views/course/PrimaryView.vue";
+
+export default defineComponent({
+  computed: {
+    PrimaryView() {
+      return PrimaryView;
+    },
+  },
+  components: {Location, IconMenu, Setting},
+  data() {
+    return {
+      imageSrc: "../../assets/tou1.jpg",
+      activeIndex: "1",
+      course:[
+          "/chinese",
+          "/math",
+          "/english",
+          "/physics",
+          "/chemistry",
+          "/political",
+          "/history",
+          "/primary",
+          "/middle",
+          "/high",
+      ]
+    };
+  },
+  methods: {
+    handleSelect(key: string, keyPath: string[]) {
+      console.log(key, keyPath);
+    },
+    Click(name: string) {
+      this.$router.push({
+        path: "/course" + name,
+        replace: true,
+      });
+    },
+    handleOpen(key: string, keyPath: string[]) {
+      console.log(key, keyPath);
+    },
+    handleClose(key: string, keyPath: string[]) {
+      console.log(key, keyPath);
+    },
+    redirectToLink(link: string) {
+      window.location.href = link;
+    },
+    applyShadow(event: {
+      currentTarget: any;
+      clientX: number;
+      clientY: number;
+    }) {
+      const container = event.currentTarget;
+      const rect = container.getBoundingClientRect();
+      const x = event.clientX - rect.left;
+      const y = event.clientY - rect.top;
+      const shadowX = (x - rect.width / 2) / 10;
+      const shadowY = (y - rect.height / 2) / 10;
+      container.style.boxShadow = `${shadowX}px ${shadowY}px 20px rgba(0, 0, 0, 0.5)`;
+    },
+    removeShadow(event: { currentTarget: { style: { boxShadow: string } } }) {
+      event.currentTarget.style.boxShadow = "";
+    },
+  },
+});
+</script>
+<style scoped>
+#menu {
+  max-width: 202px;
+  margin: 20px;
+
+  border-radius: 10px;
+}
+
+.main {
+  border: 2px solid white;
+  border-radius: 15px;
+}
+
+.header {
+  position: relative;
+  display: flex;
+  border-bottom: 2px solid white;
+  border-radius: 12px;
+  padding: 0;
+}
+
+.mymenu {
+  position: absolute;
+  left: 100px;
+}
+
+.search {
+  position: relative;
+  left: 0;
+}
+
+</style>
--- fjoj/src/router/routes.ts
+++ fjoj/src/router/routes.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { RouteRecordRaw } from "vue-router";
+import {RouteRecordRaw} from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import AdminView from "@/views/AdminView.vue";
 import NoAuthView from "@/views/NoAuthView.vue";
@@ -12,7 +12,6 @@ import postAdd from "@/views/post/postAdd.vue";
 import UserEditView from "@/views/user/UserEditView.vue";
 import ContentView from "@/views/Column/ContentView.vue";
 import ColumnView from "@/views/Column/ColumnView.vue";
-import CourseView from "@/views/course/CourseView.vue";
 import ChineseView from "@/views/course/ChineseView.vue";
 import MathView from "@/views/course/MathView.vue";
 import EnglishView from "@/views/course/EnglishView.vue";
@@ -22,135 +21,142 @@ import HistoryView from "@/views/course/HistoryView.vue";
 import PrimaryView from "@/views/course/PrimaryView.vue";
 import MiddleView from "@/views/course/MiddleView.vue";
 import HighView from "@/views/course/HighView.vue";
+import CourseLayout from "@/layouts/CourseLayout.vue";
 
 export const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/user",
-    name: "用户",
-    component: UserLayout,
-    meta: {
-      hideInMenu: true,
+    {
+        path: "/user",
+        name: "用户",
+        component: UserLayout,
+        meta: {
+            hideInMenu: true,
+        },
+        children: [
+            {
+                path: "/user/login",
+                name: "用户登录",
+                component: UserLoginAndRegisterView,
+            },
+        ],
     },
-    children: [
-      {
-        path: "/user/login",
-        name: "用户登录",
-        component: UserLoginAndRegisterView,
-      },
-      {
-        path: "/course",
-        component: CourseView,
-      },
-      {
-        path: "/course/chinese",
-        component: ChineseView,
-      },
-      {
-        path: "/course/math",
-        component: MathView,
-      },
-      {
-        path: "/course/english",
-        component: EnglishView,
-      },
-      {
-        path: "/course/physics",
-        component: PhysicsView,
-      },
-      {
-        path: "/course/chemistry",
-        component: ChineseView,
-      },
-      {
-        path: "/course/political",
-        component: PoliticalView,
-      },
-      {
-        path: "/course/history",
-        component: HistoryView,
-      },
-      {
-        path: "/course/primary",
-        component: PrimaryView,
-      },
-      {
-        path: "/course/middle",
-        component: MiddleView,
-      },
-      {
-        path: "/course/high",
-        component: HighView,
-      },
-    ],
-  },
-  {
-    path: "/",
-    name: "首页",
-    component: ContentView,
-  },
-  {
-    path: "/post",
-    name: "论坛广场",
-    component: postView,
-  },
-  {
-    path: "/column",
-    name: "专栏",
-    component: ColumnView,
-  },
-  {
-    path: "/",
-    redirect: "/chose",
-  },
-  {
-    path: "/hide",
-    name: "隐藏页面",
-    component: HomeView,
-    meta: {
-      hideInMenu: true,
+    {
+        path: "/",
+        name: "首页",
+        component: ContentView,
     },
-  },
-  {
-    path: "/noAuth",
-    name: "无权限",
-    component: NoAuthView,
-    meta: {
-      hideInMenu: true,
+    {
+        path: "/post",
+        name: "论坛广场",
+        component: postView,
     },
-  },
-  {
-    path: "/admin",
-    name: "仅管理员可见",
-    component: AdminView,
-    meta: {
-      access: accessEnum.ADMIN,
+    {
+        path: "/column",
+        name: "专栏",
+        component: ColumnView,
     },
-  },
+    {
+        path: "/",
+        redirect: "/chose",
+    },
+    {
+        path: "/hide",
+        name: "隐藏页面",
+        component: HomeView,
+        meta: {
+            hideInMenu: true,
+        },
+    },
+    {
+        path: "/noAuth",
+        name: "无权限",
+        component: NoAuthView,
+        meta: {
+            hideInMenu: true,
+        },
+    },
+    {
+        path: "/admin",
+        name: "仅管理员可见",
+        component: AdminView,
+        meta: {
+            access: accessEnum.ADMIN,
+        },
+    },
 
-  {
-    path: "/post/:postId",
-    name: "postId",
-    component: postContentView,
-    meta: {
-      hideInMenu: true,
+    {
+        path: "/post/:postId",
+        name: "postId",
+        component: postContentView,
+        meta: {
+            hideInMenu: true,
+        },
     },
-  },
-  {
-    path: "/postAdd",
-    name: "postAdd",
-    component: postAdd,
-    meta: {
-      hideInMenu: true,
-      access: accessEnum.USER,
+    {
+        path: "/postAdd",
+        name: "postAdd",
+        component: postAdd,
+        meta: {
+            hideInMenu: true,
+            access: accessEnum.USER,
+        },
     },
-  },
-  {
-    path: "/userEdit",
-    name: "userEdit",
-    component: UserEditView,
-    meta: {
-      hideInMenu: true,
-      access: accessEnum.USER,
+    {
+        path: "/userEdit",
+        name: "userEdit",
+        component: UserEditView,
+        meta: {
+            hideInMenu: true,
+            access: accessEnum.USER,
+        },
     },
-  },
+    {
+        path: "/course",
+        component: CourseLayout,
+        meta: {
+            hideInMenu: true,
+        },
+        children: [
+            {
+                path: "/course/chinese",
+                component: ChineseView,
+            },
+            {
+                path: "/course/math",
+                component: MathView,
+            },
+            {
+                path: "/course/english",
+                component: EnglishView,
+            },
+            {
+                path: "/course/physics",
+                component: PhysicsView,
+            },
+            {
+                path: "/course/chemistry",
+                component: ChineseView,
+            },
+            {
+                path: "/course/political",
+                component: PoliticalView,
+            },
+            {
+                path: "/course/history",
+                component: HistoryView,
+            },
+            {
+                path: "/course/primary",
+                component: PrimaryView,
+            },
+            {
+                path: "/course/middle",
+                component: MiddleView,
+            },
+            {
+                path: "/course/high",
+                component: HighView,
+            },
+        ]
+    },
+
 ];
