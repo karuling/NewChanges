87e646409fe11b451b522ee3fa71fc18cfdcc32e
xfrankx <2690605766@qq.com>
2023-11-24 19:13:41 +0800

    NewChanges

--- fjoj/src/views/post/postAdd.vue
+++ fjoj/src/views/post/postAdd.vue
@@ -1,27 +1,52 @@
 <template>
-  <el-form
-    ref="ruleForm"
-    :model="postAdd"
-    class="demo-ruleForm"
-    label-width="100px"
-  >
-    <el-form-item label="标题" prop="title">
-      <el-input v-model="postAdd.title"></el-input>
-    </el-form-item>
-    <el-form-item label="科目标签" prop="tags">
-      <el-checkbox-group v-model="postAdd.tags">
-        <el-checkbox label="语文"></el-checkbox>
-        <el-checkbox label="数学"></el-checkbox>
-        <el-checkbox label="英语"></el-checkbox>
-      </el-checkbox-group>
-    </el-form-item>
-    <el-form-item label="帖子内容" prop="content">
-      <el-input v-model="postAdd.content" type="textarea"></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="handleSubmitAdd()">发帖</el-button>
-    </el-form-item>
-  </el-form>
+  <div id="postAdd">
+    <el-form
+      ref="ruleForm"
+      :model="postAdd"
+      class="demo-ruleForm"
+      label-width="100px"
+    >
+      <div class="postAddOne">
+        <el-form-item label="标题" prop="title" size="large">
+          <el-input v-model="postAdd.title"></el-input>
+        </el-form-item>
+      </div>
+      <div class="postAddTwo">
+        <el-form-item label="科目标签" prop="tags" size="large">
+          <div>
+            <el-checkbox-group v-model="postAdd.tags">
+              <el-checkbox label="语文"></el-checkbox>
+              <el-checkbox label="数学"></el-checkbox>
+              <el-checkbox label="英语"></el-checkbox>
+            </el-checkbox-group>
+          </div>
+          <div>
+            <el-checkbox-group v-model="postAdd.tags" size="large">
+              <el-checkbox label="物理"></el-checkbox>
+              <el-checkbox label="化学"></el-checkbox>
+              <el-checkbox label="政治"></el-checkbox>
+              <el-checkbox label="历史"></el-checkbox>
+            </el-checkbox-group>
+          </div>
+          <div>
+            <el-checkbox-group v-model="postAdd.tags" size="large">
+              <el-checkbox label="小学"></el-checkbox>
+              <el-checkbox label="初中"></el-checkbox>
+              <el-checkbox label="高中"></el-checkbox>
+            </el-checkbox-group>
+          </div>
+        </el-form-item>
+      </div>
+      <div class="postAddThree">
+        <el-form-item label="帖子内容" prop="content" size="large">
+          <el-input v-model="postAdd.content" type="textarea"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="handleSubmitAdd()">发帖</el-button>
+        </el-form-item>
+      </div>
+    </el-form>
+  </div>
 </template>
 
 <style scoped></style>
@@ -54,3 +79,37 @@ const handleSubmitAdd = async () => {
   }
 };
 </script>
+<style>
+#postAdd {
+  display: flex;
+  height: 85vh;
+  justify-content: center;
+  align-items: center;
+  background-image: url("../../assets/background/PostAddbackground.png");
+}
+
+.demo-ruleForm {
+  height: 85vh;
+  width: 600px;
+  justify-content: center;
+  align-items: center;
+}
+
+.postAddOne {
+  margin: 15px;
+  position: relative;
+  right: 60px;
+}
+
+.postAddTwo {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 15px;
+}
+
+.postAddThree {
+  margin: 10px;
+  position: relative;
+  right: 60px;
+}
+</style>
